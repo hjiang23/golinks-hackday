1f6dcae480444cd528163073f9280d5307f39069
Howard Jiang <hjiang23@live.unc.edu>
2023-06-01 14:27:04 -0400

    added forked toggle and kb/mb/gb

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ app.set("port", process.env.PORT || 3001);
 app.get("/user", (req, res) => {
     const user = req.query.username;
     const forked = req.query.forked;
-    // console.log(forked);
 
     if (!user) {
         res.json({
@@ -17,14 +16,23 @@ app.get("/user", (req, res) => {
         return;
     }
     
-    axios.get(`https://api.github.com/search/repositories?q=user:${user}+fork:true`, {
+    let query;
+
+    if (!forked || forked === 'false') {
+        query = `https://api.github.com/search/repositories?q=user:${user}`;
+    }
+
+    else {
+        query = `https://api.github.com/search/repositories?q=user:${user}+fork:true`
+    }
+
+    axios.get(query, {
         headers: {
             'X-GitHub-Api-Version': '2022-11-28',
             'Accept' : 'application/vnd.github+json',
             // 'Authorization' : 'Bearer YOUR TOKEN'
         }
     }).then(function (response) {
-        // console.log(response.data);
         res.json({
             total_count : response.data.total_count,
             total_stargazers : getTotalStargazers(response.data.items),
@@ -39,18 +47,15 @@ app.get("/user", (req, res) => {
 });
 
 app.listen(app.get("port"), () => {
-    console.log(`Find the server at: http://localhost:${app.get("port")}/`); // eslint-disable-line no-console
+    console.log(`Find the server at: http://localhost:${app.get("port")}/`); 
 });
 
 function getTotalStargazers(items) {
-    
-    // console.log(items);
-    
+        
     let sum = 0;
     
     for (let i = 0; i < items.length; i++) {
         sum += items[i].stargazers_count;
-        // console.log(items[i]);
     }
 
     return sum;
@@ -61,7 +66,6 @@ function getTotalForks(items) {
     
     for (let i = 0; i < items.length; i++) {
         sum += items[i].forks_count;
-        // console.log(items[i]);
     }
 
     return sum;
@@ -72,21 +76,20 @@ function getAverageSize(items) {
 
     for (let i = 0; i < items.length; i++) {
         sum += items[i].size;
-        // console.log(items[i]);
     }
 
     let average = sum/items.length
 
     if (average > 1000000000) {
-        return average/1000000000 + ' GB';
+        return (average/1000000000).toFixed(2) + ' GB';
     }
 
     else if (average > 1000000) {
-        return average/1000000 + ' MB';
+        return (average/1000000).toFixed(2) + ' MB';
     }
 
     else {
-        return average/1000 + ' KB';
+        return (average/1000).toFixed(2) + ' KB';
     }
 }
 
